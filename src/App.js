f20b2ffee83bcbd1c9e31b7073df5e68428441f5
bagusrahmadani <baraelektrozone@gmail.com>
2024-03-26 20:18:38 +0700

    add home

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./components/Main";
+import icon from "./assets/icon.jpg";
+import durabilityPingThumb from "./assets/thumbnails/durability-ping.png";
+import coordinatesHudThumb from "./assets/thumbnails/coordinates-hud.png";
+import dayAndEntityCounterThumb from "./assets/thumbnails/day-and-entity-counter.png";
 
 const links = [
   {
-    id: "durability-ping",
-    title: "Durability Ping!",
-    image: "https://api.mcpedl.com/storage/submissions/102913/100/thumbnail_1-520x245.png",
+    id: "/",
+    title: "Andromedarius Download Pages",
+    image: icon,
     links: [
       {
-        name: "LootLabs [MCPACK]",
-        url: "https://loot-link.com/s?oS9t",
+        name: "Coordinates HUD",
+        url: "/download/coordinates-hud",
       },
+      {
+        name: "Durability Ping!",
+        url: "/download/durability-ping",
+      },
+      {
+        name: "Day & Entity Counter",
+        url: "/download/day-and-entity-counter",
+      },
+    ],
+  },
+  {
+    id: "/download/durability-ping",
+    title: "Durability Ping!",
+    image: durabilityPingThumb,
+    links: [
+      { name: "LootLabs [MCPACK]", url: "https://loot-link.com/s?oS9t", },
       {
         name: "LootLabs [ZIP]",
         url: "https://lootdest.com/s?oS9y",
@@ -24,6 +44,48 @@ const links = [
         name: "Linkvertise [ZIP]",
         url: "https://direct-link.net/845273/durabilitypingzipnew",
       },
+      {
+        name: "Home",
+        url: "/",
+      },
+    ],
+  },
+  {
+    id: "/download/coordinates-hud",
+    title: "Coordinates HUD",
+    image: coordinatesHudThumb,
+    links: [
+      {
+        name: "LootLabs",
+        url: "",
+      },
+      {
+        name: "Linkvertise",
+        url: "https://direct-link.net/845273/coordinates-hud-for-beta",
+      },
+      {
+        name: "Home",
+        url: "/",
+      },
+    ],
+  },
+  {
+    id: "/download/day-and-entity-counter",
+    title: "Day & Entity Counter",
+    image: dayAndEntityCounterThumb,
+    links: [
+      {
+        name: "LootLabs",
+        url: "",
+      },
+      {
+        name: "Linkvertise",
+        url: "https://link-target.net/845273/dayentitycounter-for-beta",
+      },
+      {
+        name: "Home",
+        url: "/",
+      },
     ],
   },
 ];
@@ -35,7 +97,7 @@ function App() {
         {links.map((prop) => (
           <Route
             key={prop.id}
-            path={`/andromedarius-download/${prop.id}`}
+            path={prop.id}
             element={<Main props={prop} />}
           />
         ))}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 const Main = ({ props }) => {
   return (
-    <div className="main-container"> 
+    <div className="main-container">
       <h1>{props.title}</h1>
       <img className="preview-image" src={props.image} alt={props.title} />
-      <div className="links-container"> 
+      <div className="links-container">
         {props.links.map((link) => (
           <Link key={link.name} to={link.url} className="link-button">
             {link.name}
