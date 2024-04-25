a25a9ba4fbdacf0bd10e87e9153b765049958568
bagusrahmadani <baraelektrozone@gmail.com>
2024-04-25 14:52:00 +0700

    change format system

--- src/components/Files.js
+++ src/components/Files.js
@@ -5,13 +5,13 @@ const files = [
     id: "/download/5a9e2fb5-76b6-4070-905e-c81751eef629",
     fileName: "dynamicDayCounter",
     filePath: dynamicDayCounter,
-    format: "zip",
+    format: ".zip",
   },
   {
     id: "/download/1d4b7e66-8c8a-4cb0-ab3d-9d9549977fd4",
     fileName: "dynamicDayCounter",
     filePath: dynamicDayCounter,
-    format: "mcpack",
+    format: ".mcpack",
   },
 ];
 
