7d63b5c66a4e93f87d05255f935ef03d7dd89a0a
bagusrahmadani <baraelektrozone@gmail.com>
2024-04-25 14:55:56 +0700

    formating

--- src/components/DownloadFile.jsx
+++ src/components/DownloadFile.jsx
@@ -5,13 +5,13 @@ const DownloadFile = ({ props }) => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    navigate("/");
     const downloadLink = document.createElement("a");
     downloadLink.href = props.filePath;
-    downloadLink.download = props.fileName;
+    downloadLink.download = `${props.fileName}${props.format}`;
     document.body.appendChild(downloadLink);
     downloadLink.click();
     document.body.removeChild(downloadLink);
+    navigate("/");
   }, [navigate, props]);
 
   return <div>Downloading file...</div>;
