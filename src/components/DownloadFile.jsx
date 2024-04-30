d406fdcbef770c0dcf4eb0611a9ab839a32ebb2a
bagusrahmadani <baraelektrozone@gmail.com>
2024-04-30 15:40:44 +0700

    test

--- src/components/DownloadFile.jsx
+++ src/components/DownloadFile.jsx
@@ -1,25 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const DownloadFile = ({ props }) => {
   const navigate = useNavigate();
 
-  const handleDownload = () => {
-    const downloadLink = document.createElement("a");
-    downloadLink.href = props.filePath;
-    downloadLink.download = `${props.fileName}.${props.formatFile}`;
-
-    document.body.appendChild(downloadLink);
-    downloadLink.click();
-
-    // navigate("/");
-  };
-
-  return (
-    <div>
-      <button onClick={handleDownload}>Download File</button>
-    </div>
+  useEffect(
+    () => {
+      const downloadLink = document.createElement("a");
+      const blob = new Blob([props.filePath]);
+      const url = window.URL.createObjectURL(blob);
+      downloadLink.href = url;
+      downloadLink.download = `${props.fileName}.${props.formatFile}`;
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
+      navigate("/");
+    },
+    [navigate, props],
+    []
   );
+
+  return <div>Downloading file...</div>;
 };
 
 export default DownloadFile;
--- src/components/Files.js
+++ src/components/Files.js
@@ -2,13 +2,13 @@ const files = [
   {
     id: "/download/5a9e2fb5-76b6-4070-905e-c81751eef629",
     fileName: "dynamicDayCounter",
-    filePath: "/downloads/dynamic-day-counter/DynamicDayCounter",
+    filePath: "/downloads/dynamic-day-counter/DynamicDayCounter.zip",
     formatFile: "zip",
   },
   {
     id: "/download/1d4b7e66-8c8a-4cb0-ab3d-9d9549977fd4",
     fileName: "dynamicDayCounter",
-    filePath: "/downloads/dynamic-day-counter/DynamicDayCounter",
+    filePath: "/downloads/dynamic-day-counter/DynamicDayCounter.zip",
     formatFile: "mcpack",
   },
 ];
