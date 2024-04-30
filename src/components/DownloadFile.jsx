5e3612ad21107569cfd2c9eb3b1f00dfa3e7cdd3
bagusrahmadani <baraelektrozone@gmail.com>
2024-04-30 15:11:48 +0700

    add public file

--- src/components/DownloadFile.jsx
+++ src/components/DownloadFile.jsx
@@ -4,21 +4,20 @@ import { useNavigate } from "react-router-dom";
 const DownloadFile = ({ props }) => {
   const navigate = useNavigate();
 
-  useEffect(
-    () => {
-      const downloadLink = document.createElement("a");
-      downloadLink.href = props.filePath;
-      downloadLink.download = `${props.fileName}.${props.formatFile}`;
-      document.body.appendChild(downloadLink);
-      downloadLink.click();
-      navigate("/");
-      return () => {
-        document.body.removeChild(downloadLink);
-      };
-    },
-    [navigate, props],
-    []
-  );
+  useEffect(() => {
+    const downloadLink = document.createElement("a");
+    downloadLink.href = props.filePath;
+    downloadLink.download = `${props.fileName}.${props.formatFile}`;
+
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+
+    navigate("/");
+
+    return () => {
+      document.body.removeChild(downloadLink);
+    };
+  }, [navigate, props]);
 
   return <div>Downloading file...</div>;
 };
