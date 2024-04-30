e0a12ef948e1f89f699e1d8cfca5218be1635efc
bagusrahmadani <baraelektrozone@gmail.com>
2024-04-30 16:31:55 +0700

    completed links

--- src/components/DownloadFile.jsx
+++ src/components/DownloadFile.jsx
@@ -8,8 +8,8 @@ const DownloadFile = ({ props }) => {
     const generateSerialNumber = () => {
       const today = new Date();
       const year = today.getFullYear().toString().slice(-2);
-      let month = (today.getMonth() + 1).toString().padStart(2, "0"); // Pad the month with leading zero if needed
-      const day = today.getDate().toString().padStart(2, "0"); // Pad the day with leading zero if needed
+      let month = (today.getMonth() + 1).toString().padStart(2, "0");
+      const day = today.getDate().toString().padStart(2, "0");
       const serialNumber = `${day}${month}${year}`;
       return serialNumber;
     };
@@ -18,28 +18,17 @@ const DownloadFile = ({ props }) => {
 
     const downloadFile = async () => {
       try {
-        const response = await fetch(props.filePath); // Fetch the file
-        const blob = await response.blob(); // Convert the file to Blob
-
-        // Create a URL for the Blob
+        const response = await fetch(props.filePath);
+        const blob = await response.blob();
         const url = URL.createObjectURL(blob);
 
-        // Create a download link element
         const downloadLink = document.createElement("a");
         downloadLink.href = url;
         downloadLink.download = `${props.fileName}-${serialNumber}.${props.formatFile}`;
-
-        // Append the download link to the DOM
         document.body.appendChild(downloadLink);
-
-        // Trigger the download
         downloadLink.click();
-
-        // Cleanup
         document.body.removeChild(downloadLink);
         URL.revokeObjectURL(url);
-
-        // Navigate back to the main page
         navigate("/");
       } catch (error) {
         console.error("Error downloading file:", error);
@@ -47,11 +36,6 @@ const DownloadFile = ({ props }) => {
     };
 
     downloadFile();
-
-    // Cleanup effect if needed
-    return () => {
-      // Cleanup code if needed
-    };
   }, [navigate, props]);
 
   return <div>Downloading file...</div>;
--- src/components/Files.js
+++ src/components/Files.js
@@ -1,4 +1,5 @@
 const files = [
+  //Dynamic Day Counter
   {
     id: "/download/5a9e2fb5-76b6-4070-905e-c81751eef629",
     fileName: "dynamicDayCounter",
@@ -9,6 +10,48 @@ const files = [
     id: "/download/1d4b7e66-8c8a-4cb0-ab3d-9d9549977fd4",
     fileName: "dynamicDayCounter",
     filePath: "/downloads/DynamicDayCounter.zip",
+    formatFile: "mcpack",
+  },
+
+  //Circular Hotbar
+  {
+    id: "/download/5bea0ecb-d397-487f-a5a7-dee9ce71cb8b",
+    fileName: "CircularHotbar",
+    filePath: "/downloads/CircularHotbar.zip",
+    formatFile: "zip",
+  },
+  {
+    id: "/download/6baf62e6-77ee-44cb-8992-6450f7000520",
+    fileName: "CircularHotbar",
+    filePath: "/downloads/CircularHotbar.zip",
+    formatFile: "mcpack",
+  },
+
+  //Durability Ping!
+  {
+    id: "/download/b51a384c-046a-466a-a4e5-860aa63dc9d1",
+    fileName: "DurabilityPing",
+    filePath: "/downloads/DurabilityPing.zip",
+    formatFile: "zip",
+  },
+  {
+    id: "/download/34dfee07-3f49-460d-8ff5-0d62d07398ec",
+    fileName: "DurabilityPing",
+    filePath: "/downloads/DurabilityPing.zip",
+    formatFile: "mcpack",
+  },
+
+  //Visualize Enchanted Book
+  {
+    id: "/download/92d5c15d-d128-4a61-ab9f-9e505a8cde4c",
+    fileName: "VisualizeEnchantedBook",
+    filePath: "/downloads/VisualizeEnchantedBook.zip",
+    formatFile: "zip",
+  },
+  {
+    id: "/download/aaef9ccd-8121-4e57-bc1c-3eed04fc8aa3",
+    fileName: "VisualizeEnchantedBook",
+    filePath: "/downloads/VisualizeEnchantedBook.zip",
     formatFile: "mcpack",
   },
 ];
