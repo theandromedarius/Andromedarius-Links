d19b0b8b486f7e68e275de97a319a48fbc3c26fa
bagusrahmadani <baraelektrozone@gmail.com>
2024-04-30 15:30:45 +0700

    test

--- src/components/DownloadFile.jsx
+++ src/components/DownloadFile.jsx
@@ -1,25 +1,25 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const DownloadFile = ({ props }) => {
   const navigate = useNavigate();
 
-  useEffect(() => {
+  const handleDownload = () => {
     const downloadLink = document.createElement("a");
     downloadLink.href = props.filePath;
-    downloadLink.download = `Hewahunja.mcpack`;
+    downloadLink.download = `${props.fileName}.${props.formatFile}`;
 
     document.body.appendChild(downloadLink);
     downloadLink.click();
 
-    navigate("/");
+    // navigate("/");
+  };
 
-    return () => {
-      document.body.removeChild(downloadLink);
-    };
-  }, [navigate, props]);
-
-  return <div>Downloading file...</div>;
+  return (
+    <div>
+      <button onClick={handleDownload}>Download File</button>
+    </div>
+  );
 };
 
 export default DownloadFile;
