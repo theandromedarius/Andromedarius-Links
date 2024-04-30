47e306dc196c4e33df454fe7c164b8fb06e66a7c
bagusrahmadani <baraelektrozone@gmail.com>
2024-04-30 16:05:51 +0700

    octet-stream add

--- src/components/DownloadFile.jsx
+++ src/components/DownloadFile.jsx
@@ -6,10 +6,22 @@ const DownloadFile = ({ props }) => {
 
   useEffect(
     () => {
+      const generateSerialNumber = () => {
+        const today = new Date();
+        const year = today.getFullYear().toString().slice(-2);
+        let month = (today.getMonth() + 1).toString();
+        month = month.length === 1 ? "0" + month : month;
+        const day = today.getDate().toString();
+        const serialNumber = day + month + year;
+        return serialNumber;
+      };
+
+      const serialNumber = generateSerialNumber();
+
       const downloadLink = document.createElement("a");
       downloadLink.href = props.filePath;
-      downloadLink.download = `${props.fileName}.${props.formatFile}`;
-      downloadLink.type = "application/zip";
+      downloadLink.download = `${props.fileName}-${serialNumber}.${props.formatFile}`;
+      downloadLink.type = "application/octet-stream";
       document.body.appendChild(downloadLink);
       downloadLink.click();
       document.body.removeChild(downloadLink);
