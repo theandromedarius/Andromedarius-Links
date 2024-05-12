6d1482c526afc4dd7ed3fa4340f008260edb16c0
bagusrahmadani <baraelektrozone@gmail.com>
2024-05-12 16:26:43 +0700

    20240511

--- src/components/RedirectLink.jsx
+++ src/components/RedirectLink.jsx
@@ -62,36 +62,6 @@ const RedirectLink = ({ props }) => {
       );
     }
 
-    const serialNumber = () => {
-      const today = new Date();
-      const year = today.getFullYear().toString().slice(-2);
-      const month = (today.getMonth() + 1).toString().padStart(2, "0");
-      const day = today.getDate().toString().padStart(2, "0");
-      const serialNumber = `${day}${month}${year}`;
-      return serialNumber;
-    };
-
-    const downloadFile = async () => {
-      try {
-        const response = await fetch(props.filePath);
-        const blob = await response.blob();
-        const url = URL.createObjectURL(blob);
-
-        const downloadLink = document.createElement("a");
-        downloadLink.href = url;
-        downloadLink.type = "application/octet-stream";
-        downloadLink.download = `${props.fileName}-${serialNumber()}.${
-          props.formatFile
-        }`;
-        document.body.appendChild(downloadLink);
-        downloadLink.click();
-        document.body.removeChild(downloadLink);
-        URL.revokeObjectURL(url);
-      } catch (error) {
-        console.error("Error downloading file:", error);
-      }
-    };
-
     const observer = new MutationObserver((mutationsList) => {
       for (let mutation of mutationsList) {
         if (
@@ -108,8 +78,6 @@ const RedirectLink = ({ props }) => {
     });
 
     observer.observe(adsContainerRef.current, { childList: true });
-
-    downloadFile();
   }, [props]);
 
   useEffect(() => {
